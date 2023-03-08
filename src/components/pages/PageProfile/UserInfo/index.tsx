1452fd8fa5db6f63baf079eb70808b479b00699d
FINIKKKK <digitallkrash@gmail.com>
2023-03-08 23:44:04 +0600

    edit user style

--- src/components/pages/PageProfile/UserInfo/index.tsx
+++ src/components/pages/PageProfile/UserInfo/index.tsx
@@ -1,4 +1,5 @@
 import ss from "./UserInfo.module.scss";
+import { useSelectors } from "@/hooks/useSelectors";
 import { TUser } from "@/utils/api/models/user/types";
 import React from "react";
 
@@ -7,37 +8,86 @@ interface UserInfoProps {
 }
 
 export const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  const { data: userData } = useSelectors((state) => state.user);
+  const isAuthor = userData?.id === user.id;
+  const [isEdit, setIsEdit] = React.useState(false);
+  const [nameValue, setNameValue] = React.useState(user.name);
+  const [locationValue, setLocationValue] = React.useState(user.location);
+
+  const onEditContent = () => {
+    if (!isEdit) {
+      setIsEdit(true);
+    } else {
+      setIsEdit(false);
+    }
+  };
+
   return (
     <div className={`user__info block ${ss.user}`}>
-      <img
-        src={
-          user.avatar !== null
-            ? `http://localhost:7777/img/avatars/${user.avatar}`
-            : `../img/avatar.png`
-        }
-        alt="avatar"
-        className={`avata ${ss.avatar}`}
-      />
+      <div className={ss.avatar}>
+        <img
+          src={
+            user.avatar
+              ? `http://localhost:7777/img/avatars/${user.avatar}`
+              : `../img/avatar.png`
+          }
+          alt="avatar"
+        />
+        {isAuthor && (
+          <div className={ss.avatar__edit}>
+            <svg width="20" height="20">
+              <use xlinkHref="../img/icons/icons.svg#edit" />
+            </svg>
+          </div>
+        )}
+      </div>
       <div className={ss.info}>
-        {user.firstName !== null ||
-          (user.lastName !== null && (
-            <div className={ss.name}>
-              {user.firstName} {user.lastName}
-            </div>
-          ))}
-        <h6 className={ss.login}>@{user.login}</h6>
+        {!isEdit ? (
+          user.name && <div className={ss.name}>{user.name}</div>
+        ) : (
+          <input
+            value={nameValue}
+            onChange={(e) => setNameValue(e.target.value)}
+            className={`${ss.input} ${ss.inputName}`}
+            placeholder="Имя"
+            type="text"
+          />
+        )}
+        {!isEdit && <h6 className={ss.login}>@{user.login}</h6>}
       </div>
       <div className={ss.extra}>
-        <div className={ss.box}>
-          <div className={ss.label}>Местоположение</div>
-          <p className={ss.item}>City 17</p>
-        </div>
-        <div className={ss.box}>
-          <div className={ss.label}>Email</div>
-          <p className={ss.item}>krashmate@gmail.com</p>
-        </div>
+        {isEdit ? (
+          <input
+            value={locationValue}
+            onChange={(e) => setLocationValue(e.target.value)}
+            className={`${ss.input} ${ss.inputLocation}`}
+            placeholder="Местоположение"
+            type="text"
+          />
+        ) : (
+          user.location && (
+            <div className={ss.box}>
+              <div className={ss.label}>Местоположение</div>
+              <p className={ss.item}>{user.location}</p>
+            </div>
+          )
+        )}
+        {!isEdit && (
+          <div className={ss.box}>
+            <div className={ss.label}>Email</div>
+            <p className={ss.item}>{user.email}</p>
+          </div>
+        )}
       </div>
-      <button className={`btn ${ss.subscribe}`}>Подписаться</button>
+      <div className={ss.footer}>
+        {isAuthor ? (
+          <button onClick={onEditContent} className={`btn ${ss.edit}`}>
+            {!isEdit ? "Редактировать" : "Сохранить"}
+          </button>
+        ) : (
+          <button className={`btn ${ss.subscribe}`}>Подписаться</button>
+        )}
+      </div>
     </div>
   );
 };
--- src/utils/api/models/user/types.ts
+++ src/utils/api/models/user/types.ts
@@ -8,12 +8,12 @@ export type TUser = TBase & {
   login: string;
   email: string;
   password: string;
-  firstName?: string;
-  lastName?: string;
+  name?: string;
   avatar?: string;
   token: string;
   questionCount: number;
   answerCount: number;
+  location?: string;
 };
 
 export type ParamsUserDto = ParamsDto & {
